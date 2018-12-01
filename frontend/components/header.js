d2225f13153a6954fd163541aa07ca49e321d3bf
Hafiz Didarali <hafizdidarali@gmail.com>
2018-12-01 20:02:29 +0530

    progress bar and loading functioanlity

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -1,7 +1,18 @@
 import Link from "next/link";
 import styled from "styled-components";
 import Nav from "./nav";
+import Router from "next/router";
+import NProgress from "nprogress";
 
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+Router.onRouteChangeError = () => {
+  console.log("onRouteChangeError");
+};
 const Logo = styled.h1`
   font-size: 4rem;
   margin-left: 2rem;
